cad6d38565974c85d2dfce0849476d164a67a7ec
PaulyB <43163391+3saster@users.noreply.github.com>
2018-10-12 18:44:19 -0700

    Added tracers to WordCount_1

--- Lab3/wordCount_1.cpp
+++ Lab3/wordCount_1.cpp
@@ -2,6 +2,11 @@
 #include <fstream>
 #include <map>
 #include <string>
+#include <cstdlib>
+#include <csignal>
+
+#include "tracelib.h"
+#define LOGGING 1 //Set to one to turn on tracing
 
 template<typename Map>
 void print_map(const Map& m)
@@ -12,6 +17,16 @@ void print_map(const Map& m)
 
 int main(int argc, char *argv[])
 {
+    /*    START LOGGING    */
+    #if LOGGING
+        trace::trace_start("wordCount_1.json");
+        auto traceEnd = [] (int i) { trace::trace_instant_global("Program Aborted"); trace::trace_end(); exit(128+i); };
+        std::signal(SIGINT, traceEnd); //^C
+        std::signal(SIGABRT, traceEnd); //abort()
+        std::atexit([](){ trace::trace_end(); }); //return
+    #endif
+
+    trace::trace_event_start("Word Count 1","Word Count 1");
     // Check for argument
     if(argc < 2)
     {
@@ -20,6 +35,7 @@ int main(int argc, char *argv[])
     }
 
     // Open file
+    trace::trace_event_start("Open File","Word Count 1");
     std::ifstream textFile;
     textFile.open(argv[1]);
     if(!textFile.is_open())
@@ -27,16 +43,23 @@ int main(int argc, char *argv[])
         std::cerr << "Error: Unable to open file \"" << argv[1] << "\".\n";
         return 1;
     }
+    trace::trace_event_end();
 
     // Count Words
+    trace::trace_event_start("Count Words","Word Count 1");
     std::string word;
     std::map<std::string, int> wordCount;
     while( textFile >> word )
     {
         wordCount[word]++;
     }
+    trace::trace_event_end();
 
     // Print Map
+    trace::trace_event_start("Print Word Count","Word Count 1");
     print_map(wordCount);
+    trace::trace_event_end();
+
+    trace::trace_event_end();
     return 0;
 }
