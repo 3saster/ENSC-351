6ce210f2113f63d13d06209350f1959a33732545
PaulyB <43163391+3saster@users.noreply.github.com>
2018-10-13 02:04:10 -0700

    Threaded wordCount_2, using a threadpool. I wish I knew a better way to do this... Threadpool library used according to license conditions

--- Lab3/wordCount_2.cpp
+++ Lab3/wordCount_2.cpp
@@ -6,60 +6,117 @@
 #include <vector>
 #include <utility>
 #include <algorithm>
+#include <mutex>
 
+#include "threadpool/ctpl_stl.h"
 #include "tracelib.h"
 #define LOGGING 1 //Set to one to turn on tracing
+#define NUM_THREADS std::thread::hardware_concurrency()-1 //Number of effective additional cores
 
 typedef std::string data;
 typedef std::pair<data,int> kvPair;
+std::mutex mtx;
 
-std::vector<data> input_reader(std::ifstream& textFile)
+/*    MapReduce Components    */
+std::vector<data> input_reader(std::ifstream& textFile, int tid=1)
 {
-    trace::trace_event_start("Input Reader","MapReduce");
+    trace::trace_event_start("Input Reader","MapReduce",tid);
 
-    std::string word;
-    std::vector<std::string> wordVector;
+    data word;
+    std::vector<data> wordVector;
     while( textFile >> word )
     {
         wordVector.push_back(word);
     }
 
-    trace::trace_event_end();
+    trace::trace_event_end(tid);
 
     return wordVector;
 }
 
-kvPair map(data word)
+kvPair map(data word, int tid=1)
 {
-    trace::trace_event_start("Map","MapReduce");
+    trace::trace_event_start("Map","MapReduce",tid);
 
     auto value = kvPair (word,1);
 
-    trace::trace_event_end();
+    trace::trace_event_end(tid);
     return value;
 }
 
-kvPair reduce(std::vector<kvPair> groupVector)
+kvPair reduce(std::vector<kvPair> groupVector, int tid=1)
 {
-    trace::trace_event_start("Reduce","MapReduce");
+    trace::trace_event_start("Reduce","MapReduce",tid);
 
     kvPair reducedKV (groupVector.front().first,0);
     for(auto& kv : groupVector)
         reducedKV.second += kv.second;
 
-    trace::trace_event_end();
+    trace::trace_event_end(tid);
     return reducedKV;
 }
 
 template<typename Map>
-void output(const Map& m)
+void output(const Map& m, int tid=1)
 {
-    trace::trace_event_start("Output","MapReduce");
+    trace::trace_event_start("Output","MapReduce",tid);
 
     for(auto& p: m)
         std::cout << p.first << " : " << p.second << std::endl;
 
-    trace::trace_event_end();
+    trace::trace_event_end(tid);
+}
+
+/*    Threading Stuff    */
+void thread_map(int tid, data element, std::vector<kvPair>& pairVector)
+{
+    auto mappedElement = map(element, tid+2);
+    mtx.lock();
+    pairVector.push_back( mappedElement );
+    mtx.unlock();
+}
+
+void thread_reduce(int tid, std::vector<kvPair> groupVector, std::vector<kvPair>& reducedVector)
+{
+    auto pushPair = reduce(groupVector, tid+2);
+    mtx.lock();
+    reducedVector.push_back( pushPair );
+    mtx.unlock();
+}
+
+void mapReduce(std::ifstream& textFile)
+{
+    std::vector<data> dataVector;
+    std::vector<kvPair> pairVector;
+    std::vector<kvPair> reducedVector;
+    ctpl::thread_pool tp(NUM_THREADS);
+
+    dataVector = input_reader(textFile);
+    for(auto& element : dataVector)
+    {
+        tp.push(thread_map, element,std::ref(pairVector));
+    }
+    while(tp.n_idle() != NUM_THREADS){} //Spinlock until threads are done mapping
+
+    std::sort (pairVector.begin(), pairVector.end());
+    auto groupFront = pairVector.begin();
+    auto groupBack  = groupFront;
+    while (groupBack <= pairVector.end())
+    {
+        if(groupFront->first == groupBack->first) //If data is the same
+            groupBack++;
+        else
+        {
+            std::vector<kvPair> shortVector (groupFront,groupBack);
+            tp.push(thread_reduce, shortVector,std::ref(reducedVector));
+            groupFront = groupBack;
+        }
+    }
+
+    while(tp.n_idle() != NUM_THREADS){} //Spinlock until threads are done mapping
+
+    std::sort(reducedVector.begin(), reducedVector.end());
+    output(reducedVector);
 }
 
 int main(int argc, char *argv[])
@@ -92,35 +149,9 @@ int main(int argc, char *argv[])
     }
     trace::trace_event_end();
 
-    /*    GENERIC STUFF    */
+    /*    MapReduce    */
     trace::trace_event_start("Count Words","Word Count 2");
-
-    std::vector<data> dataVector;
-    std::vector<kvPair> pairVector;
-    std::vector<kvPair> reducedVector;
-
-    dataVector = input_reader(textFile);
-    for(auto& element : dataVector)
-    {
-        pairVector.push_back( map(element) );
-    }
-    std::sort (pairVector.begin(), pairVector.end());
-    auto groupFront = pairVector.begin();
-    auto groupBack  = groupFront;
-    while (groupBack <= pairVector.end())
-    {
-        if(groupFront->first == groupBack->first) //If data is the same
-            groupBack++;
-        else
-        {
-            std::vector<kvPair> shortVector (groupFront,groupBack);
-            kvPair pushPair = reduce( shortVector );
-            reducedVector.push_back(pushPair);
-            groupFront = groupBack;
-        }
-    }
-    output(reducedVector);
-
+    mapReduce(textFile);
     trace::trace_event_end();
 
     // End process
