33c0f50a82b26d7883f8c1b4d6aae8ae68117f3f
pbologea <pbologea@ensc-esil-10.ensc.sfu.ca>
2018-10-26 13:45:20 -0700

    Added logging. Hard to add it in with openmp, so it was added in with std::thread instead.

--- Lab5/SATSolve.cpp
+++ Lab5/SATSolve.cpp
@@ -1,5 +1,36 @@
 #include <iostream>
+#include <thread>
+#include <chrono>
 #include "SAT_Prob.h"
+
+#ifndef LOGTIMER
+    #define LOGTIMER 2
+#endif
+
+#define DIV 200
+
+bool done=false;
+
+void logger(SAT_Problem &sat)
+{
+    int i;
+    float step = 1000 * LOGTIMER / DIV;
+    //done must be checked many times due to the nature of threading
+    while(!done) 
+    {
+        i=0;
+        //Trick to not sleep the full LOGTIMER amount if the SAT is solved early
+        while( i < DIV && !done)
+        {
+            std::this_thread::sleep_for( std::chrono::milliseconds( int(step) ) );
+            i++;
+        }
+        if(!done)
+        {
+            std::cout << sat.GetBacktracks() << " backtracks done so far.\r" << std::flush;
+        }
+    }
+}
 
 int main(int argc, char *argv[])
 {
@@ -21,15 +52,24 @@ int main(int argc, char *argv[])
 
     /*    SAT Problem Solver    */
     SAT_Problem sat(textFile);
+    //Start Logger
+    std::thread log(logger,std::ref(sat));
+    
+    //SAT Solver
     if( sat.Solve() )
     {
-        std::cout << "A solution was found. Note that the left-most number is variable 1, and X's are don't-cares." << std::endl << std::endl;
+        done = true;
+        std::cout << "A solution was found after " << sat.GetBacktracks() << " backtracks." << std::endl
+                  << "Note that the left-most number is variable 1, and X's are don't-cares." << std::endl << std::endl;
         sat.Print();
     }
     else
     {
+        done = true;
         std::cout << "No solution could be found." << std::endl;
     }
+    
+    log.join();
 
     // End process
     return 0;
