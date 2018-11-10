99a9882291047606fe510c3ee3057d720601573b
PaulyB <43163391+3saster@users.noreply.github.com>
2018-11-10 02:24:02 -0800

    Added much more descriptive comments

--- Lab5/SAT_Prob.cpp
+++ Lab5/SAT_Prob.cpp
@@ -25,6 +25,7 @@ SAT_Problem::SAT_Problem (std::ifstream& textFile)
     textFile >> word;
     vars.assign(word, Unset);
 
+    //Read each clause into clauses member
     textFile >> word;
     int clause=1;
     while( clause <= word )
@@ -38,6 +39,11 @@ SAT_Problem::SAT_Problem (std::ifstream& textFile)
     }
 }
 
+/*
+Copy Constructor
+
+Needed since atomic does not have a copy constructor.
+*/
 SAT_Problem::SAT_Problem(const SAT_Problem& sat)
 {
     vars = sat.vars;
@@ -82,13 +88,17 @@ bool_SAT SAT_Problem::Check()
 {
     std::vector<bool_SAT> clauseResults ( clauses.size(), Unset );
 
+    //For each clause...
     for (unsigned int i=0; i<clauses.size(); i++)
     {
         std::vector<bool_SAT> clauseOutput;
         bool ORskip = false;
 
+        //For each variable in this clause...
         for( auto var:clauses[i] )
         {
+            //...get the value of that part of the clause (negating if necessary)
+            // and push it into the output vector
             auto value = vars[abs(var)-1];
             value = var > 0 ? value : NOT(value);
             //If value is True, this clause will be True, so leave early
@@ -101,6 +111,7 @@ bool_SAT SAT_Problem::Check()
             clauseOutput.push_back(value);
         }
 
+        //OR the results of each variable in the clause
         if(!ORskip)
         {
             auto clauseValue = OR(clauseOutput);
@@ -112,6 +123,7 @@ bool_SAT SAT_Problem::Check()
         }
     }
 
+    //AND the results of the clauses
     return AND(clauseResults);
 }
 
@@ -123,7 +135,8 @@ true if a solution is found, otherwise return false.
 */
 bool SAT_Problem::Solve()
 {
-    //Get depth of parallel binary tree search (min of either round down number of cores, or number of variables )
+    //Get depth of parallel binary tree search (min of either round down number of cores, or number of variables)
+    //i.e. if we have 4-7 cores, we can do 2 levels deep (4 branches)
     int threadDepth = std::min( log2(std::thread::hardware_concurrency()), double( vars.size() ) );
     int threadNum = pow(2,threadDepth);
 
@@ -132,29 +145,41 @@ bool SAT_Problem::Solve()
     solved=false;
     for( auto &v:vars )
         v=Unset;
-    //Backtrack search
+    //Backtrack search passed to group of threads
     std::thread *threads = new std::thread[threadNum];
     for(int tid=0; tid<threadNum; tid++)
     {
         threads[tid] = std::thread(&SAT_Problem::threadSolve,this, tid,threadDepth);
     }
 
+    //Wait for threads to rejoin
     for(int tid=0; tid<threadNum; tid++)
         threads[tid].join();
-
     delete [] threads;
 
+    //Return true or false depending on if a solution was found
     if(solved)
         return true;
     else
         return false;
 }
 
+/*
+threadSolve()
+
+This performs a backtrack search on a copy of the SAT_Problem, where certain variables
+are locked.
+
+For example, given a thread depth of 3 (i.e 8 cores), thread 0 does a backtrack search
+where the first three values are locked to 000, thread 1 does 100, thread 2 does 010, etc.
+*/
 bool SAT_Problem::threadSolve(int tid, int threadDepth)
 {
+    //Create copy of SAT problem and its vars
     SAT_Problem copySAT = *this;
     auto *copyVars = &copySAT.vars;
-    //Lock starting values
+
+    //Lock starting values in
     for(int i=0;i<threadDepth;i++)
         (*copyVars)[i] = (tid>>i)%2 ? True : False;
 
@@ -162,7 +187,6 @@ bool SAT_Problem::threadSolve(int tid, int threadDepth)
     int i=threadDepth;
     while ( !solved && i <= int( (*copyVars).size() ) )
     {
-        //this->Print();
         switch( copySAT.Check() )
         {
             //We have a solution
@@ -175,7 +199,7 @@ bool SAT_Problem::threadSolve(int tid, int threadDepth)
                 (*copyVars)[i] = True;
                 i++;
                 break;
-            //Backtrack to first "True" Value
+            //Backtrack to first (not locked) "True" Value
             case False:
                 i--;
                 while( i >= threadDepth)
@@ -193,12 +217,12 @@ bool SAT_Problem::threadSolve(int tid, int threadDepth)
                         backtracks++;
                     }
                 }
-                if (i<threadDepth) return false; //No solution exists (backtracked past front)
+                if (i<threadDepth) return false; //No solution exists (backtracked past locked values)
                 break;
         }
         if(solved) return true;
     }
     //Should exit in switch case; if not, something went wrong
-    std::cerr << "Error in SAT_Problem::Solve(): Exited in an unexpected way." << std::endl;
+    std::cerr << "Error in SAT_Problem::threadSolve() on thread " << tid << ": Exited in an unexpected way." << std::endl;
     return false;
 }
